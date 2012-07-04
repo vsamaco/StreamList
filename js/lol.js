4891ea9631bcef5d3c0c343f4e9b3929752c6ecc
Vincent Samaco <vsamaco@gmail.com>
2012-07-03 17:10:19 -0700

    fixed this.options initialize function

--- js/lol.js
+++ js/lol.js
@@ -53,7 +53,7 @@ $(function(){
       this.model.bind('change', this.render, this);
       this.model.bind('destroy', this.remove, this);
       this.model.bind('change:online', this.unrender, this);
-      this.toggle_unrender = false || options.toggle_unrender;
+      this.toggle_unrender = false || this.options.toggle_unrender;
     },
     
     render: function() {
